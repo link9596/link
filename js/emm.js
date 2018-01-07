830d7d7267fb46f418b3738cd549e234a60e634e
Link <1316341442@qq.com>
2018-01-07 08:13:45 +0000

    Update emm.js

--- js/emm.js
+++ js/emm.js
@@ -20,14 +20,4 @@ window.location.href="http://lkopp.ml";
  else{
    alert("用户名或密码错误!");
   }
-  var user2 = "lin", password2 = "888";
-  var temUser2 = document.form1.username.value;
-  var temPassword2 = document.form1.password.value;
-  if(user2==temUser2 && password2==temPassword2)
-  {
-    alert("账户登入成功!");window.location.href="http://lkopp.ml";
-  }
-  else{
-    alert("用户名或密码错误!");
-  }
 }
