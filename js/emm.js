dd481e26d38591cf8a216e3b23e59e6d9f572215
Link <1316341442@qq.com>
2018-01-07 13:50:28 +0800

    Add files via upload

---
+++ js/emm.js
@@ -0,0 +1,23 @@
+function KeyDown()
+{
+  if (event.keyCode == 13)
+  {
+    event.returnValue=false;
+    event.cancel = true;
+    Form1.btnsubmit.click();
+  }
+}
+
+function check(){
+ var user = "link", password = "95969639";
+ var temUser = document.form1.username.value;
+ var temPassword = document.form1.password.value;
+ if(user==temUser && password==temPassword)
+{
+ alert("登入成功!");
+window.location.href="http://lkopp.ml";
+}
+ else{
+   alert("用户名或密码错误!");
+  }
+}
