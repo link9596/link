bc16103017f75b1172a78c97d5667e746f19bbae
Link <1316341442@qq.com>
2018-01-13 16:11:56 +0000

    Update emm.js

--- js/emm.js
+++ js/emm.js
@@ -27,7 +27,7 @@ function check(){
 window.location.href="http://lkopp.ml";
 }
  else{
-   document.getElementById('alert').style.transform='translateY(85px)';
+   document.getElementById('alert').style.transform='translateY(78px)';
    //alert("用户名或密码错误!");
   }
 }
